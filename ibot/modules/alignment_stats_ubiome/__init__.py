bbd94c3771433b3044e258772dfd5097585f5a74
David Danko <dcdanko@gmail.com>
2016-06-07 13:31:39 +0100

    renamed dirs

--- ibot/modules/alignment_stats_ubiome/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-
-from .alignment_stats_ubiome import IBotModule
--- ibot/modules/alpha_diversity/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-
-from .alpha_diversity import IBotModule
